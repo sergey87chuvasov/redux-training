5ed22c6a382abc646bbb38815ee5b01c971cd2e3
Sergey87 <serge.techno@gmail.com>
2023-11-13 22:21:52 +0300

    added redux trainingadded redux training - redux toolkit - 2 part

--- bogdan-redux/2-reduxPureProject/index.js
+++ bogdan-redux/2-reduxPureProject/index.js
@@ -1,41 +1,62 @@
 import store from './redux/store.js';
+import getCurrentTime from './utils/getCurrentTime.js';
 
-// console.log(store);
-// console.log(store.getState()); // [] - first state of our app
+// // console.log(store);
+// // console.log(store.getState()); // [] - first state of our app
 
-const unsubscribe = store.subscribe(() =>
-  console.log(`Redux store just changed! ${store.getState()}`)
-);
-/**
- Redux store just changed! 11:33:25
- Redux store just changed! 11:33:25,11:44:33
- Redux store just changed! 
- */
+// const unsubscribe = store.subscribe(() =>
+//   console.log(`Redux store just changed! ${store.getState()}`)
+// );
+// /*
+//  Redux store just changed! 11:33:25
+//  Redux store just changed! 11:33:25,11:44:33
+//  Redux store just changed!
+//  */
 
-store.dispatch({
-  type: 'ADD_CURRENT_TIME',
-  payload: '11:33:25',
+// store.dispatch({
+//   type: 'ADD_CURRENT_TIME',
+//   payload: '11:33:25',
+// });
+
+// // if we use unsubscribe
+// unsubscribe();
+// /*
+
+// /*
+//  Redux store just changed! 11:33:25
+// */
+
+// // console.log(store.getState()); // ['11:33:25']
+
+// store.dispatch({
+//   type: 'ADD_CURRENT_TIME',
+//   payload: '11:44:33',
+// });
+
+// // console.log(store.getState()); // (2) ['11:33:25', '11:44:33']
+
+// store.dispatch({
+//   type: 'CLEAR_ALL_TIMES',
+// });
+
+// // console.log(store.getState()); // []
+
+const addTimeBtn = document.getElementById('addTime');
+addTimeBtn.addEventListener('click', () => {
+  store.dispatch({
+    type: 'ADD_CURRENT_TIME',
+    payload: getCurrentTime(),
+  });
 });
 
-// if we use unsubscribe
-unsubscribe();
-/*
+const timesList = document.getElementById('timesList');
+store.subscribe(() => {
+  timesList.innerHTML = '';
+  const times = store.getState();
+  times.forEach((time) => {
+    let li = document.createElement('li');
+    li.innerText = time;
 
-/*
- Redux store just changed! 11:33:25
-*/
-
-// console.log(store.getState()); // ['11:33:25']
-
-store.dispatch({
-  type: 'ADD_CURRENT_TIME',
-  payload: '11:44:33',
+    timesList.appendChild(li);
+  });
 });
-
-// console.log(store.getState()); // (2) ['11:33:25', '11:44:33']
-
-store.dispatch({
-  type: 'CLEAR_ALL_TIMES',
-});
-
-// console.log(store.getState()); // []
